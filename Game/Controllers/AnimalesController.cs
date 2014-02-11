a081cc381f2831ad44c174d0aa7c86b29a89dec0
MartinezCatriel <martinezcatriel@gmail.com>
2014-02-11 00:37:14 -0300

    changes

--- Game/Controllers/AnimalesController.cs
+++ Game/Controllers/AnimalesController.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using Prediccion;
 
 namespace Game.Controllers
 {
     public class AnimalesController : ApiController
     {
 
-        public HttpResponseMessage Get()
+        public HttpResponseMessage Get(string id)
         {
-            
-
-            return new HttpResponseMessage();
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            var animales = new List<Animal>();
+            animales.Add(AnimalCreator.GetAnimalInstanceByType(TipoAnimal.Leon, 1, "leon choton"));
+            animales.Add(AnimalCreator.GetAnimalInstanceByType(TipoAnimal.Leon, 2, "pulpo paul"));
+            response.Content = new ObjectContent(typeof(List<Animal>), animales, new JsonMediaTypeFormatter()); // new StringContent("{equipoid:1, equiponombre:argentina}");
+            return response;
         }
     }
 }
