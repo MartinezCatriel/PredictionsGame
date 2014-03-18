3604eb721d8c09776a1f8abd0a23fe398097780c
Catriel <catriel@batanga.com>
2014-03-18 17:17:12 -0300

    changes

---
+++ Repository/EntitiesRepository/PartidoRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.EntitiesRepository
+{
+    public class PartidoRepository
+    {
+        public IRepository Repo { get; private set; }
+        public PartidoRepository(IRepository repo)
+        {
+            Repo = repo;
+        }
+
+        public void GetAll()
+        {
+            Repo.GetAll<Partido>();
+        }
+
+        public void Update(object unPartido)
+        {
+            //Repo.SearchFor<Partido>((uno) => {return uno. });
+        }
+    }
+}
